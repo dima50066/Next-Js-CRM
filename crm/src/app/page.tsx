76850b0b4f55950519190500e628d5034a724009
Dumitru <dima50066@gmail.com>
2024-12-24 13:17:31 +0100

    work with styles tailwind clsx

--- crm/src/app/page.tsx
+++ crm/src/app/page.tsx
@@ -1,7 +1,13 @@
+import StatusLabel, { Status } from './components/status-label';
+
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1 className="text-3xl">Home</h1>
+      <StatusLabel status={Status.Active}>Active</StatusLabel>
+      <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
+      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
     </div>
   );
 }
