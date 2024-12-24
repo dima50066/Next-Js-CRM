85105c7c8cbe98f2fde5cdf98486ae588b557621
Dumitru <dima50066@gmail.com>
2024-12-24 14:07:00 +0100

    test rendering

--- crm/src/app/page.tsx
+++ crm/src/app/page.tsx
@@ -1,13 +1,17 @@
-import StatusLabel, { Status } from './components/status-label';
+import AddCompanyButton from './components/add-company-button';
+import ClientComponent from './components/client-component';
+import ServerComponent from './components/server-component';
+import ServerComponentCopy from './components/server-component copy';
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-3xl">Home</h1>
-      <StatusLabel status={Status.Active}>Active</StatusLabel>
-      <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
-      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+      <AddCompanyButton />
+      <ServerComponent />
+      <ClientComponent>
+        <ServerComponentCopy />
+      </ClientComponent>
     </div>
   );
 }
