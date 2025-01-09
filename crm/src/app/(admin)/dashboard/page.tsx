8191a26f96a6c23b6daa9a9581168f8cf6a54825
Dumitru <dima50066@gmail.com>
2025-01-09 13:02:45 +0100

    Parallel Routes

--- crm/src/app/(admin)/dashboard/page.tsx
+++ crm/src/app/(admin)/dashboard/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import Header from '@/app/components/header';
 
-export interface PageProps {}
-
+export interface PageProps {
+  title: string;
+}
 export default function Page({}: PageProps) {
   return (
-    <main>
-      <h1 className="text-xl">Dashboard Page</h1>
-    </main>
+    <>
+      <Header>Dashboard</Header>
+    </>
   );
 }
